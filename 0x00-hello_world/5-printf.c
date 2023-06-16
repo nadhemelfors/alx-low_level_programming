7e5460df40ce8a672f946135fcf6bcc1aecc3192
nadhemelfors <enadhem@gmail.com>
2023-06-16 15:43:07 -0700

    azert

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*main - program print a line using printf
+*Return: 0 (Succes)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
